52d8c5c4f04c5a86339d0e12e3702489f0610cd0
Jasmine <jasqxl94@gmail.com>
2019-07-12 01:20:24 +0800

    [JasQ] Add 4 digits validator for number

--- src/main/kotlin/Validator.kt
+++ src/main/kotlin/Validator.kt
@@ -1,4 +1,9 @@
 class Validator {
+    fun isFourDigitString(string: String): Boolean {
+        val fourDigitRegex = """(^\s*[0-9]\s*[0-9]\s*[0-9]\s*[0-9]\s*$)""".toRegex()
+        return fourDigitRegex.matches(string)
+    }
+
     fun isStringWithUniqueChar(string: String): Boolean {
         return string
             .map{ char -> string.count { char == it } == 1 }
--- src/test/kotlin/ValidatorTest.kt
+++ src/test/kotlin/ValidatorTest.kt
@@ -1,24 +1,61 @@
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class ValidatorTest {
     private var validator = Validator()
 
-    @Test
-    fun shouldReturnTrueIfStringIsEmpty() {
-        val result = validator.isStringWithUniqueChar("")
-        Assertions.assertTrue(result)
+    @Nested
+    inner class IsFourDigitString {
+        @Test
+        fun shouldReturnFalseIfStringIsEmpty() {
+            val result = validator.isFourDigitString("")
+            Assertions.assertFalse(result)
+        }
+
+        @Test
+        fun shouldReturnTrueIfStringIsHasFourDigits() {
+            val result = validator.isFourDigitString("1111")
+            Assertions.assertTrue(result)
+        }
+
+        @Test
+        fun shouldReturnTrueIfStringIsHasFourDigitsWithSpaces() {
+            val result = validator.isFourDigitString(" 1 1 1 1 ")
+            Assertions.assertTrue(result)
+        }
+
+        @Test
+        fun shouldReturnFalseIfStringHasMoreThanFourDigits() {
+            val result = validator.isFourDigitString("11111")
+            Assertions.assertFalse(result)
+        }
+
+        @Test
+        fun shouldReturnFalseIfStringHasANonDigitChar() {
+            val result = validator.isFourDigitString("d111")
+            Assertions.assertFalse(result)
+        }
     }
 
-    @Test
-    fun shouldReturnTrueIfStringIsUnique() {
-        val result = validator.isStringWithUniqueChar("29r4")
-        Assertions.assertTrue(result)
-    }
+    @Nested
+    inner class IsStringWithUniqueChar {
+        @Test
+        fun shouldReturnTrueIfStringIsEmpty() {
+            val result = validator.isStringWithUniqueChar("")
+            Assertions.assertTrue(result)
+        }
 
-    @Test
-    fun shouldReturnFalseIfStringIsNotUnique() {
-        val result = validator.isStringWithUniqueChar("w2i3w")
-        Assertions.assertFalse(result)
+        @Test
+        fun shouldReturnTrueIfStringIsUnique() {
+            val result = validator.isStringWithUniqueChar("29r4")
+            Assertions.assertTrue(result)
+        }
+
+        @Test
+        fun shouldReturnFalseIfStringIsNotUnique() {
+            val result = validator.isStringWithUniqueChar("w2i3w")
+            Assertions.assertFalse(result)
+        }
     }
 }
