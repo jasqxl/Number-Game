e7b8790d1938aa1161ad47cc5dcac2dcf5150462
Jasmine <jasmine.quah@thoughtworks.com>
2019-08-21 20:05:23 +0800

    [JasQ] Refactor test

--- src/test/kotlin/NumberUtilsTest.kt
+++ src/test/kotlin/NumberUtilsTest.kt
@@ -1,20 +1,11 @@
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.lang.IllegalArgumentException
 import org.junit.jupiter.api.assertThrows
 
 class NumberUtilsTest {
     private var numberUtils = NumberUtils()
-
-    @Test
-    fun shouldGenerateUniqueFourDigitString() {
-        val result = numberUtils.generateUniqueFourDigits()
-        Assertions.assertEquals(4, result.length)
-        Assertions.assertTrue(result
-            .map{ char -> result.count { char == it } == 1 }
-            .all { it }
-        )
-    }
 
     @Test
     fun shouldNotThrowErrorIfInputIsValid() {
@@ -28,20 +19,36 @@ class NumberUtilsTest {
         }
     }
 
-    @Test
-    fun shouldThrowNotFourDigitsException() {
-        val exception = assertThrows<IllegalArgumentException>{
-            numberUtils.checkInput( Number("12in"))
+    @Nested
+    inner class GenerateUniqueFourDigits {
+        @Test
+        fun shouldGenerateUniqueFourDigitString() {
+            val result = numberUtils.generateUniqueFourDigits()
+            Assertions.assertEquals(4, result.length)
+            Assertions.assertTrue(result
+                .map{ char -> result.count { char == it } == 1 }
+                .all { it }
+            )
         }
-        Assertions.assertEquals("Input is not a 4 digit string, please try again", exception.message)
     }
 
-    @Test
-    fun shouldThrowNotUniqueFourDigitsException() {
-        val exception = assertThrows<IllegalArgumentException>{
-            numberUtils.checkInput( Number("1222"))
+    @Nested
+    inner class CheckInput {
+        @Test
+        fun shouldThrowNotFourDigitsException() {
+            val exception = assertThrows<IllegalArgumentException>{
+                numberUtils.checkInput( Number("12in"))
+            }
+            Assertions.assertEquals("Input is not a 4 digit string, please try again", exception.message)
         }
 
-        Assertions.assertEquals("Input is not a unique string, please try again", exception.message)
+        @Test
+        fun shouldThrowNotUniqueFourDigitsException() {
+            val exception = assertThrows<IllegalArgumentException>{
+                numberUtils.checkInput( Number("1222"))
+            }
+
+            Assertions.assertEquals("Input is not a unique string, please try again", exception.message)
+        }
     }
 }
